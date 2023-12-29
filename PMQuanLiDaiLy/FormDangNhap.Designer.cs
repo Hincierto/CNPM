fd07c5f71331f179898f0f8ea0b94d2212d51e77
UIT-21522078 <21522078@gm.uit.edu.vn>
2023-12-29 14:35:21 +0700

    source PMQuanLyDaiLy

---
+++ PMQuanLiDaiLy/FormDangNhap.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace PMQuanLiDaiLy
+{
+    partial class FormDangNhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txTKDangNhap = new System.Windows.Forms.TextBox();
+            this.txMKDangNhap = new System.Windows.Forms.TextBox();
+            this.btDangNhap = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(137, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(172, 41);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Đăng nhập";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(56, 223);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tài khoản:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(56, 297);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 28);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Mật khẩu:";
+            // 
+            // txTKDangNhap
+            // 
+            this.txTKDangNhap.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txTKDangNhap.Location = new System.Drawing.Point(160, 225);
+            this.txTKDangNhap.Name = "txTKDangNhap";
+            this.txTKDangNhap.Size = new System.Drawing.Size(225, 30);
+            this.txTKDangNhap.TabIndex = 3;
+            // 
+            // txMKDangNhap
+            // 
+            this.txMKDangNhap.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txMKDangNhap.Location = new System.Drawing.Point(160, 297);
+            this.txMKDangNhap.Name = "txMKDangNhap";
+            this.txMKDangNhap.Size = new System.Drawing.Size(225, 30);
+            this.txMKDangNhap.TabIndex = 4;
+            this.txMKDangNhap.UseSystemPasswordChar = true;
+            // 
+            // btDangNhap
+            // 
+            this.btDangNhap.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btDangNhap.Location = new System.Drawing.Point(311, 397);
+            this.btDangNhap.Name = "btDangNhap";
+            this.btDangNhap.Size = new System.Drawing.Size(134, 53);
+            this.btDangNhap.TabIndex = 5;
+            this.btDangNhap.Text = "Đăng nhập";
+            this.btDangNhap.UseVisualStyleBackColor = true;
+            this.btDangNhap.Click += new System.EventHandler(this.btDangNhap_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(276, 354);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(109, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Đăng kí tài khoản";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::PMQuanLiDaiLy.Properties.Resources.profile;
+            this.pictureBox1.Location = new System.Drawing.Point(163, 97);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(105, 104);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FormDangNhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 516);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btDangNhap);
+            this.Controls.Add(this.txMKDangNhap);
+            this.Controls.Add(this.txTKDangNhap);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormDangNhap";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FormDangNhap";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txTKDangNhap;
+        private System.Windows.Forms.TextBox txMKDangNhap;
+        private System.Windows.Forms.Button btDangNhap;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
